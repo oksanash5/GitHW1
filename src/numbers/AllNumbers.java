5ec3372850aef65a69b9130efafc96761974d989
oksanash5 <oksanash55@gmail.com>
2022-08-30 01:11:45 -0400

    print all numbers

---
+++ src/numbers/AllNumbers.java
@@ -0,0 +1,10 @@
+package numbers;
+
+public class AllNumbers {
+    public static void main(String[] args) {
+        //print all numbers from 1 to 100
+        for(int i=1; i<=100; i++){
+            System.out.print(i +" ");
+        }
+    }
+}
